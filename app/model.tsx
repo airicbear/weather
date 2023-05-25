522268697b8a9d7b2493eaffc5e4a14cb098f037
Eric Nguyen <etndevel@gmail.com>
2023-05-25 15:59:49 -0400

    Add data fetch via SWR (#3)

---
+++ app/model.tsx
@@ -0,0 +1,25 @@
+export default interface WeatherResponse {
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  current_weather: {
+    temperature: number;
+    windspeed: number;
+    winddirection: number;
+    weathercode: number;
+    is_day: number;
+    time: string;
+  };
+  hourly_units: {
+    time: string;
+    temperature_2m: string;
+  };
+  hourly: {
+    time: string[];
+    temperature_2m: number[];
+  };
+}
